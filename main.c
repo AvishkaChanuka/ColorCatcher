48af7a8688baa64caacaf31069e46d20d78db150
Avishka Chanuka <86764648+AvishkaChanuka@users.noreply.github.com>
2023-07-22 07:44:32 +0530

    Initialize game window and set screen dementions
    Width = 800
    Height 450

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
-#include <stdio.h>
+#include "raylib.h"
 
-int main() {
-    printf("Hello, World!\n");
+int main(){
+
+    //Initialization
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth,screenHeight,"Color Catcher");
+
+    SetTargetFPS(60);
+
+    while(!WindowShouldClose()){
+
+        //Update
+
+        //Draw
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        EndDrawing();
+    }
+
+    //DeInitialization
+    CloseWindow();
+
     return 0;
 }
