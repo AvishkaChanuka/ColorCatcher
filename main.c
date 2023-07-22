d8b3d081a2934683437f61f312b5f57b3363d864
Avishka Chanuka <86764648+AvishkaChanuka@users.noreply.github.com>
2023-07-22 10:09:17 +0530

    Create player and setup player movement
    A or Left Arrow key -> move left
    W or Right Arrow key -> move right

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
 #include "raylib.h"
 
+/*
+ *******************************************
+ * typedefs and structures
+ *******************************************
+*/
+
+//Player
+typedef struct Player{
+    Vector2 position;
+    int width, height;
+    Color color;
+    float speed;
+}Player;
+
 int main(){
 
     /*
@@ -14,8 +28,19 @@ int main(){
     InitWindow(screenWidth,screenHeight,"Color Catcher");
     SetTargetFPS(60);
 
+    //Player
+    Player player;
+    player.width = 80;
+    player.height = 20;
+    player.position.x = (float)screenWidth/2 - (float)player.width/2;
+    player.position.y = (float)(screenHeight - 50 - player.height);
+    player.color = DARKGRAY;
+    player.speed = 300;
+
 
     while(!WindowShouldClose()){
+
+        float deltaTime = GetFrameTime();
 
         /*
          *******************************************
@@ -23,12 +48,29 @@ int main(){
          *******************************************
         */
 
+        if(IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)){
+            player.position.x -= player.speed * deltaTime;
+        }
+
+        if(IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)){
+            player.position.x += player.speed * deltaTime;
+        }
+
         /*
          *******************************************
          * Update the scene
          *******************************************
         */
 
+        //Set player in the screen boundary
+        if(player.position.x < 0){
+            player.position.x = 0;
+        }
+
+        float maxPlayerX = (float)screenWidth-(float)player.width;
+        if(player.position.x > maxPlayerX){
+            player.position.x = maxPlayerX;
+        }
 
         /*
          *******************************************
@@ -38,6 +80,9 @@ int main(){
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
+
+        //Draw player
+        DrawRectangle((int)player.position.x,(int)player.position.y,player.width,player.height,player.color);
 
         EndDrawing();
     }
